b960dae9991a54e86c41d68b8847314fff674087
tgmeng <tgmeng@163.com>
2020-06-29 14:22:35 +0800

    refactor(icon): better icon style

--- src/components/Icon/style.ts
+++ src/components/Icon/style.ts
@@ -3,5 +3,7 @@ import styled from '@emotion/styled';
 export const styleCommonIcon = (IconComponent: SVGRComponent) => styled(
   IconComponent
 )`
+  display: inline-block;
+  line-height: 1;
   vertical-align: -0.125em;
 `;
