1c8637c72c09199d75e9f53801f102ac869614b1
tgmeng <tgmeng@163.com>
2020-07-02 13:31:20 +0800

    refactor(button): modify Alert & Button file structure

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,8 +7,6 @@ import { ReactComponent as RawReloadIcon } from '../../resources/svgs/reload.svg
 import { ButtonProps } from './type';
 import { Button as StyledButton, ReloadIconStyle, IconSpan } from './style';
 
-export { ButtonProps };
-
 const ReloadIcon = styleCommonIcon(RawReloadIcon);
 
 const Button: React.FC<ButtonProps> = ({ children, loading, ...props }) => (
