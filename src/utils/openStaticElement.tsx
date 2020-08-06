21c78d74ad1451e6d55c164326f1d402a1f0f2be
tgmeng <tgmeng@163.com>
2020-08-06 12:49:45 +0800

    feat(util): add openStaticElement

---
+++ src/utils/openStaticElement.tsx
@@ -0,0 +1,70 @@
+import * as ReactDOM from 'react-dom';
+
+export interface OpenableProps {
+  isOpen: boolean;
+  onClose?: () => void;
+}
+
+export interface StaticElement<P> {
+  update(props: Partial<React.PropsWithChildren<P>>): void;
+  close(): void;
+}
+
+export default function openStaticElement<P extends OpenableProps>(
+  renderElement: (state: P) => React.ReactElement<P>,
+  initialState?: Partial<P>
+): StaticElement<P> {
+  let mountNode: HTMLDivElement | null = document.createElement('div');
+
+  let state = {
+    ...initialState,
+    isOpen: true,
+  } as P;
+
+  function render(props: P) {
+    requestAnimationFrame(() => {
+      ReactDOM.render(
+        ReactDOM.createPortal(
+          renderElement({
+            ...props,
+            onClose: () => {
+              props.onClose?.();
+              // eslint-disable-next-line @typescript-eslint/no-use-before-define
+              close();
+            },
+          }),
+          document.body
+        ),
+        mountNode
+      );
+    });
+  }
+
+  function update(props: Partial<P>) {
+    state = {
+      ...state,
+      ...props,
+    };
+
+    render(state);
+  }
+
+  function destroy() {
+    requestAnimationFrame(() => {
+      ReactDOM.unmountComponentAtNode(mountNode as HTMLDivElement);
+      mountNode = null;
+    });
+  }
+
+  function close() {
+    update({ isOpen: false } as P);
+    destroy();
+  }
+
+  render(state);
+
+  return {
+    update,
+    close,
+  };
+}
