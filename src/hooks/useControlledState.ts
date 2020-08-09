330d312d4d52d0a6a412c052b3e4d4323b0516a8
tgmeng <tgmeng@163.com>
2020-08-09 22:20:43 +0800

    feat(hooks): useControlledState

---
+++ src/hooks/useControlledState.ts
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+export default function useControlledState<S = undefined>(
+  state?: S,
+  handleStateChange?: (state: S) => void,
+  initialState?: S
+): [S | undefined, React.Dispatch<React.SetStateAction<S>>] {
+  const [uncontrolledState, setUncontrolledState] = React.useState<
+    S | undefined
+  >(initialState);
+
+  const isControlled = state !== undefined;
+
+  const setState = React.useCallback(
+    (action) => {
+      setUncontrolledState(action);
+      handleStateChange?.(
+        typeof action === 'function'
+          ? action(isControlled ? state : uncontrolledState)
+          : action
+      );
+    },
+    [isControlled, state, uncontrolledState, handleStateChange]
+  );
+
+  return [isControlled ? state : uncontrolledState, setState];
+}
