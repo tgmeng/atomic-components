05a023549c9cc1caa2391cecc66b6baaba543152
tgmeng <tgmeng@163.com>
2020-08-20 13:16:31 +0800

    feat(types): Intent & OpenableProps

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,8 @@
 export type SVGRComponent = React.FC<React.SVGAttributes<SVGElement>>;
+
+export type Intent = 'info' | 'success' | 'danger' | 'warning';
+
+export interface OpenableProps {
+  isOpen: boolean;
+  onClose?: () => void;
+}
