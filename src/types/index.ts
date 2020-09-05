26374458bbfe100ddf95c065add541cc36f659a6
tgmeng <tgmeng@163.com>
2020-09-05 18:17:00 +0800

    feat(types): Size

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export type SVGRComponent = React.FC<React.SVGAttributes<SVGElement>>;
 
 export type Intent = 'info' | 'success' | 'danger' | 'warning';
+export type Size = 'small' | 'normal' | 'large';
 
 export interface OpenableProps {
   isOpen: boolean;
