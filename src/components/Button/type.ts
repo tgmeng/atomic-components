45b37cb34c56ce063e003ec76717a08d8b3b3b4a
tgmeng <tgmeng@163.com>
2020-09-05 18:18:46 +0800

    refactor(button): use types/Intent

--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -1,6 +1,6 @@
-import { Intent as CommonIntent } from '../../types';
+import { Intent as CommonIntent, Size as CommonSize } from '../../types';
 
-export type Size = 'small' | 'normal' | 'large';
+export type Size = CommonSize;
 export type Variant = 'default' | 'primary' | 'dashed' | 'link';
 export type Intent = CommonIntent;
 
