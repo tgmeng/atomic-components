c6bd011b6f7e10a9b64ec60532c3eb577e054213
tgmeng <tgmeng@163.com>
2020-11-10 08:58:35 +0800

    config: 配置测试环境

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,22 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { modules: false }],
-    '@babel/preset-react',
-    '@emotion/babel-preset-css-prop',
-  ],
+const baseConfig = {
+  presets: ['@babel/preset-react', '@emotion/babel-preset-css-prop'],
+};
+
+module.exports = (api) => {
+  const isTest = api.env('test');
+
+  return isTest
+    ? {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          '@babel/preset-typescript',
+          ...baseConfig.presets,
+        ],
+      }
+    : {
+        presets: [
+          ['@babel/preset-env', { modules: false }],
+          ...baseConfig.presets,
+        ],
+      };
 };
